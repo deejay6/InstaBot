90e7f71ad8a6ea68ee65d610ba73e9b7d1c10727
deejay6 <dhgambhir@gmail.com>
2017-07-10 03:48:57 +0530

    Like and Unlike Media

--- instabot.py
+++ instabot.py
@@ -1,7 +1,7 @@
 import requests, urllib
 from termcolor import colored
 from access_token import ACCESS_TOKEN
-from user_detail import User, user_list
+from user_detail import User, user_list,Recent_Media, media_list
 BASE_URL = 'https://api.instagram.com/v1/'
 
 # Function retrieve owner's information
@@ -60,15 +60,16 @@ def select_user():
     else:
         index = 1
         for i in range(0, len(user_list)):
-            print '%d %s' % (index, user_list[i].name)
+            print user_list
+            print '%d.  %s' % (index, user_list[i].name)
             index = index + 1
-            while True:
-                choice = raw_input("Enter Your Choice: ")
-                try:
-                    choice = int(choice)
-                    break
-                except:
-                    print colored("Enter Valid Choice", "red")
+        while True:
+            choice = raw_input("Enter Your Choice: ")
+            try:
+                choice = int(choice)
+                break
+            except:
+                print colored("Enter Valid Choice", "red")
         choice = int(choice)
         return choice - 1
 
@@ -109,7 +110,7 @@ def fetch_user_details():
 # Function to fetch  self recent posts.
 
 
-def fetch_recent_posts():
+def fetch_own_recent_posts():
     count = int(raw_input("Enter number of posts you want to retrieve: "))
     try:
         request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s&count=%d') % (ACCESS_TOKEN, count)
@@ -140,32 +141,86 @@ def fetch_user_recent_posts():
     index = select_user()
     if index is not None:
         id1 = user_list[index].id
-        count = int(raw_input("Enter number of posts you want to retrieve: "))
-        try:
-            request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s&count=%d') % (id1, ACCESS_TOKEN, count)
-            print 'GET request url : %s' % (request_url)
-            user_media = requests.get(request_url).json()
-            if user_media['meta']['code'] == 200:
-                if len(user_media['data']):
-                    if len(user_media['data']) >= count:
-                        for i in range(0, count):
-                            image_name = user_media['data'][i]['id'] + '.jpeg'
-                            image_url = user_media['data'][i]['images']['standard_resolution']['url']
-                            urllib.urlretrieve(image_url, image_name)
-                            print 'Your image has been downloaded!'
+        name = user_list[index].name
+        if not media_list:
+            count = int(raw_input("Enter number of posts you want to retrieve: "))
+            try:
+                request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s&count=%d') % (id1, ACCESS_TOKEN, count)
+                print 'GET request url : %s' % (request_url)
+                user_media = requests.get(request_url).json()
+                if user_media['meta']['code'] == 200:
+                    if len(user_media['data']):
+                        if len(user_media['data']) >= count:
+                            for i in range(0, count):
+                                media_id = user_media['data'][i]['id']
+                                media_type = user_media['data'][i]['type']
+                                media_url = user_media['data'][i]['images']['standard_resolution']['url']
+                                media_likes = user_media['data'][i]['likes']['count']
+                                media_list.append(Recent_Media(name, media_id, media_type, media_url, media_likes))
+                                ch = raw_input("Do you want to download image (y/n): ")
+                                if ch.upper() == 'Y':
+                                    image_name = user_media['data'][i]['id'] + '.jpeg'
+                                    urllib.urlretrieve(media_url, image_name)
+                                    print 'Your image has been downloaded!'
+                        else:
+                            print "Your count limit exceeds bye user total posts"
                     else:
-                        print "Your count limit exceeds bye user total posts"
-                    print user_media
+                        print 'Post does not exist!'
                 else:
-                    print 'Post does not exist!'
-            else:
-                print 'Status code other than 200 received!'
-        except:
-            print "Something wrong with url"
+                    print 'Status code other than 200 received!'
+            except:
+                print "Something wrong with url"
     else:
         print "Please Add Users"
 
 
+def like_recent_post():
+    index = select_user()
+    name = user_list[index].name
+    if index is not None:
+        for i in range(0, len(media_list)):
+            if media_list[i].name == name:
+                media_id = media_list[i].media_id
+                try:
+                    request_url = (BASE_URL + 'media/%s/likes') % (media_id)
+                    print 'GET request url : %s' % (request_url)
+                    payload = {'access_token': ACCESS_TOKEN}
+                    post_like = requests.post(request_url, payload).json()
+                    if post_like['meta']['code'] == 200:
+                        print "You have liked the pic successfully!!"
+                        break
+                    else:
+                        print "Status code not 200!"
+                        break
+                except:
+                    print "Url request exception occurred!!!"
+                    break
+    else:
+        print "Please add users!!"
+
+
+def delete_like():
+    index = select_user()
+    name = user_list[index].name
+    if index is not None:
+        for i in range(0, len(media_list)):
+            if media_list[i].name == name:
+                media_id = media_list[i].media_id
+                try:
+                    request_url = (BASE_URL + 'media/%s/likes?access_token=%s') % (media_id, ACCESS_TOKEN)
+                    print 'GET request url : %s' % (request_url)
+                    post_like = requests.delete(request_url).json()
+                    if post_like['meta']['code'] == 200:
+                        print "You have unliked the pic successfully!!"
+                        break
+                    else:
+                        print "Status code not 200!"
+                        break
+                except:
+                    print "Url request exception occurred!!!"
+                    break
+    else:
+        print "Please add users!!"
 
 
 def start():
@@ -178,8 +233,8 @@ def start():
         print "3.Fetch User Details"
         print "4.Fetch your own recent post"
         print "5.Fetch the recent post of a user"
-        # print "e.Ge a list of people who have liked the recent post of a user\n"
-        # print "f.Like the recent post of a user\n"
+        print "6.Like the most recent post of a user"
+        print "7.Unlike the most recent post of a user"
         # print "g.Get a list of comments on the recent post of a user\n"
         # print "h.Make a comment on the recent post of a user\n"
         # print "i.Delete negative comments from the recent post of a user\n"
@@ -199,10 +254,14 @@ def start():
         elif choice == 3:
             fetch_user_details()
         elif choice == 4:
-            fetch_recent_posts()
+            fetch_own_recent_posts()
         elif choice == 5:
             fetch_user_recent_posts()
         elif choice == 6:
+            like_recent_post()
+        elif choice == 7:
+            delete_like()
+        elif choice == 8:
             exit()
         else:
             print "Enter valid option"
